ae9dc335b2976d23bd74961d4e9873464000724f
Christopher Davenport <chris@christopherdavenport.tech>
2021-03-15 20:31:59 -0700

    Comment Examples

--- examples/src/main/scala/BloomExample.scala
+++ examples/src/main/scala/BloomExample.scala
@@ -7,11 +7,11 @@ object BloomExample extends IOApp {
   def run(args: List[String]): IO[ExitCode] = {
     val x = "Foo"
     for {
-      bf <- BloomFilter.string[IO](10000, 0.01)
-      present1 <- bf.mayContain(x)
+      bf <- BloomFilter.string[IO](numberOfItems = 10000,  falsePositiveRate = 0.01)
+      present1 <- bf.mayContain(x) // False - It hasn't been inserted yet
       _ <- IO(println(present1))
       _ <- bf.add(x)
-      present2 <- bf.mayContain(x)
+      present2 <- bf.mayContain(x) // True - It was inserted
       _ <- IO(println(present2))
     } yield ExitCode.Success
   }
--- examples/src/main/scala/CuckooExample.scala
+++ examples/src/main/scala/CuckooExample.scala
@@ -6,14 +6,14 @@ object CuckooExample extends IOApp {
   def run(args: List[String]): IO[ExitCode] = {
     val x = "Foo"
     for {
-      cf <- CuckooFilter.string[IO](10000, 0.01)
-      present1 <- cf.mayContain(x)
+      cf <- CuckooFilter.string[IO](numberOfItems = 10000,  falsePositiveRate = 0.01)
+      present1 <- cf.mayContain(x) // False - It hasn't been inserted yet
       _ <- IO(println(present1))
       _ <- cf.add(x)
-      present2 <- cf.mayContain(x)
+      present2 <- cf.mayContain(x) // True - It was inserted
       _ <- IO(println(present2))
       _ <- cf.remove(x)
-      present3 <- cf.mayContain(x)
+      present3 <- cf.mayContain(x) // False - It was removed again. Cool!
       _ <- IO(println(present3))
     } yield ExitCode.Success
   }
